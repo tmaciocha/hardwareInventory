3b312fffae1cb657ab2dde5b2b786db09ccfc3b5
tmaciocha <tmaciocha@gmail.com>
2022-01-22 12:48:18 +0100

    <groupId>ch.qos.logback</groupId>   <artifactId>logback-classic</artifactId>   added

--- src/main/java/pl/tm/hardwareinventory/controller/HomeController.java
+++ src/main/java/pl/tm/hardwareinventory/controller/HomeController.java
@@ -1,14 +1,30 @@
 package pl.tm.hardwareinventory.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+                                    /*@RequestMapping("/sample-logger")
+                                    @ResponseBody
+                                    public String sample() {
+                                    logger.info("simple log");
+                                    int a = 2;
+                                    int b = 5;
+                                    logger.debug("a = {}, b = {} ", a, b);
+                                    return "sample";
+                                    }*/
+
+
 
     @RequestMapping("/")
     public String homePage(){
+
+        logger.info("!!!!!!wywołany homepage!!!!!!");
         return"index";
     }
 }
